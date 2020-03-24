05574975e68a143841ee36148af9ad5f86044d2d
Barata Estevao <barataestevaomario@gmail.com>
2020-03-24 03:20:00 -0700

    first commit

---
+++ src/main/java/com/example/Teste_Verificado/TesteVerificadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Teste_Verificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVerificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteVerificadoApplication.class, args);
+	}
+
+}
