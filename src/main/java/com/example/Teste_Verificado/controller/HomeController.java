f598efb5c1a66df153df73fe1f8bb460f0b782e6
Barata Estevao <barataestevaomario@gmail.com>
2020-04-13 01:23:04 -0700

    sss

---
+++ src/main/java/com/example/Teste_Verificado/controller/HomeController.java
@@ -0,0 +1,49 @@
+package com.example.Teste_Verificado.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView model = new ModelAndView("index");
+        model.addObject("message", "Hello Yose");
+
+        return model;
+    }
+
+    @GetMapping("/leame")
+    public ModelAndView leame(){
+
+        ModelAndView model = new ModelAndView("leame");
+        return model;
+    }
+
+    @GetMapping("/contactme")
+    public ModelAndView contactMe(){
+
+        ModelAndView model = new ModelAndView("contactme");
+        return model;
+    }
+
+    @GetMapping("/astroport")
+    public ModelAndView astrop(){
+
+        ModelAndView model = new ModelAndView("astroport");
+        return model;
+    }
+
+
+
+    @GetMapping("/ primeFactors?")
+    public ModelAndView primeFactors(){
+
+        ModelAndView model = new ModelAndView("primeFactors");
+        return model;
+    }
+
+
+}
